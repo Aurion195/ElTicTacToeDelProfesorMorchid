8b61ac8af5b144780915b20b497206379b2657be
Thomas Mathieu <mathieuthomas84300@hotmail.fr>
2019-03-02 12:37:08 +0100

    Refactorinf all Button function

--- src/View/MainAppViewController.java
+++ src/View/MainAppViewController.java
@@ -25,99 +25,68 @@ public class MainAppViewController
 	Image croix = new Image("Images/1487086345-cross_81577.png") ;
 	Game game = new Game();
 	
-	private boolean turnPlayer1 = false ;
-	private Vector<Boolean> casePlayed = new Vector<Boolean>();
-	private Vector<String> choicePlayeurStrings = new Vector<String>() ;
-	
-	public MainAppViewController() 
-	{
-		for(int i = 0 ; i < 9 ; i++)
-		{
-			this.casePlayed.add(true) ;
-			int j = i ;
-			this.choicePlayeurStrings.add(String.valueOf(j)) ;
-		}
-	}
 
 	@FXML
 	void onClickButton00()
 	{
 		this.Button00.setDisable(true);
-		this.Button00.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(0, (this.turnPlayer1 ? "x" : "o"));
+		this.Button00.setImage((this.game.turnPlayer(0) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton01()
 	{
 		this.Button01.setDisable(true);
-		this.Button01.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(1, (this.turnPlayer1 ? "x" : "o"));
+		this.Button01.setImage((this.game.turnPlayer(1) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton02()
 	{
 		this.Button02.setDisable(true);
-		this.Button02.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(2, (this.turnPlayer1 ? "x" : "o"));
+		this.Button02.setImage((this.game.turnPlayer(2) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton10()
 	{
 		this.Button10.setDisable(true);
-		this.Button10.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(3, (this.turnPlayer1 ? "x" : "o"));
+		this.Button10.setImage((this.game.turnPlayer(3) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton11()
 	{
 		this.Button11.setDisable(true);
-		this.Button11.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(4, (this.turnPlayer1 ? "x" : "o"));
+		this.Button11.setImage((this.game.turnPlayer(4) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton12()
 	{
 		this.Button12.setDisable(true);
-		this.Button12.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(5, (this.turnPlayer1 ? "x" : "o"));
+		this.Button12.setImage((this.game.turnPlayer(5) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton20()
 	{
 		this.Button20.setDisable(true);
-		this.Button20.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(6, (this.turnPlayer1 ? "x" : "o"));
+		this.Button20.setImage((this.game.turnPlayer(6) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton21()
 	{
 		this.Button21.setDisable(true);
-		this.Button21.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(7, (this.turnPlayer1 ? "x" : "o"));
+		this.Button21.setImage((this.game.turnPlayer(7) ? cercle : croix)) ;
 	}
 	
 	@FXML
 	void onClickButton22()
 	{
 		this.Button22.setDisable(true);
-		this.Button22.setImage((turnPlayer1 ? cercle : croix)) ;
-		this.turnPlayer1 = !this.turnPlayer1 ;
-		this.choicePlayeurStrings.set(8, (this.turnPlayer1 ? "x" : "o"));
+		this.Button22.setImage((this.game.turnPlayer(8) ? cercle : croix)) ;
 	}
 	
 }
