ab5e752e0df3d37609c664a3394a0fd7cd05ee0b
Aurion195 <mathieuthomas84300@hotmail.fr>
2019-03-04 14:57:13 +0100

    add Singleton main

--- src/Game/Launcher.java
+++ src/Game/Launcher.java
@@ -6,13 +6,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.* ;
 import javafx.stage.Stage;
+import sun.launcher.resources.launcher;
 
 public class Launcher extends Application 
 {
 
     private Stage primaryStage;
     private AnchorPane rootLayout;
+	private static volatile Launcher instance = null;
 
+	public Launcher()
+	{
+	    super();
+	    synchronized(Launcher.class)
+	    {
+	        if(instance != null) throw new UnsupportedOperationException(
+	                getClass()+" is singleton but constructor called more than once");
+	        instance = this;
+	    }
+	}
+
+	public final static Launcher getInstance()
+	{
+		if(Launcher.instance == null)
+		{
+			synchronized(Launcher.class)
+			{
+				if(Launcher.instance == null)
+				{
+					Launcher.instance = new Launcher();
+				}
+			}
+		}
+		return Launcher.instance;
+	}
+	
     @Override
     public void start(Stage primaryStage) 
     {
@@ -30,7 +58,7 @@ public class Launcher extends Application
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Launcher.class.getClassLoader().getResource("View/MainAppView.fxml"));
+            loader.setLocation(Launcher.class.getClassLoader().getResource("View/MenuView.fxml"));
             rootLayout = (AnchorPane) loader.load();
             
             // Show the scene containing the root layout.
@@ -45,9 +73,39 @@ public class Launcher extends Application
         }
     }
 
- 
-    
     /**
+	 * @return the rootLayout
+	 */
+	public AnchorPane getRootLayout() 
+	{
+		return rootLayout;
+	}
+
+	/**
+	 * @param primaryStage the primaryStage to set
+	 */
+	public void setPrimaryStage(Stage primaryStage) 
+	{
+		this.primaryStage = primaryStage;
+	}
+
+	/**
+	 * @param rootLayout the rootLayout to set
+	 */
+	public void setRootLayout(AnchorPane rootLayout) 
+	{
+		this.rootLayout = rootLayout;
+	}
+
+	/**
+	 * @param instance the instance to set
+	 */
+	public static void setInstance(Launcher instance) 
+	{
+		Launcher.instance = instance;
+	}
+
+	/**
      * Returns the main stage.
      * @return
      */
