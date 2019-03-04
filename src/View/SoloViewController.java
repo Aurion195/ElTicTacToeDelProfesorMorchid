563fec3df7c45f4dbc46d7e3d14794b4eafdf34d
Aurion195 <mathieuthomas84300@hotmail.fr>
2019-03-04 17:15:30 +0100

    Add SoloViewController

---
+++ src/View/SoloViewController.java
@@ -0,0 +1,31 @@
+package View;
+
+import Game.Launcher;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+public class SoloViewController 
+{
+	
+	@FXML
+	void onClickHomeButton()
+	{
+		Launcher main = Launcher.getInstance();
+    	FXMLLoader loader = new FXMLLoader();
+    	try {
+    		loader.setLocation(getClass().getResource("MenuView.fxml"));
+    		//loader.setLocation(getClass().getResource("winner.fxml"));
+    		main.setRootLayout(loader.load());
+
+    		Scene scene = new Scene(main.getRootLayout());
+    		main.getPrimaryStage().setScene(scene);
+    		main.getPrimaryStage().show();
+    	}
+    	catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+}
