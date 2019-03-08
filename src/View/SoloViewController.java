fab0a9d6dceacaedd9b6f4abb93ca07168f8c458
Aurion195 <mathieuthomas84300@hotmail.fr>
2019-03-08 15:34:31 +0100

    add soloView Controller

--- src/View/SoloViewController.java
+++ src/View/SoloViewController.java
@@ -1,33 +1,32 @@
 package View;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import Game.Launcher;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 
-public class SoloViewController 
+public class SoloViewController extends ToolsBarController
 {
 	private TextField namePlayer ;
 	
 	@FXML
-	void onClickHomeButton()
+	public void onClickHomeButton()
 	{
-		Launcher main = Launcher.getInstance();
-    	FXMLLoader loader = new FXMLLoader();
-    	try {
-    		loader.setLocation(getClass().getResource("MenuView.fxml"));
-    		//loader.setLocation(getClass().getResource("winner.fxml"));
-    		main.setRootLayout(loader.load());
-
-    		Scene scene = new Scene(main.getRootLayout());
-    		main.getPrimaryStage().setScene(scene);
-    		main.getPrimaryStage().show();
-    	}
-    	catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		super.onClickHomeButton();
 	}
 	
+	@FXML
+	public void onClickQuitButton()
+	{
+		super.onClickQuitButton();
+	}
 	
+	@FXML
+	public void onClickSetttingButton()
+	{
+		super.onClickSettingButton();
+	}
 }
