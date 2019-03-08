10a064211e6c8247090649f4839d2f137188ad74
Aurion195 <mathieuthomas84300@hotmail.fr>
2019-03-08 15:34:16 +0100

    add toolView controller

---
+++ src/View/ToolViewController.java
@@ -0,0 +1,18 @@
+package View;
+
+import javafx.fxml.FXML;
+
+public class ToolViewController extends ToolsBarController
+{
+	@FXML
+	public void onClickHomeButton()
+	{
+		super.onClickHomeButton();
+	}
+	
+	@FXML
+	public void onClickQuitButton()
+	{
+		super.onClickQuitButton();
+	}
+}
