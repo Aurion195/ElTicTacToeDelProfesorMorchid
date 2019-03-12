578144cc70ccb37701ceb3da6a759b25a3631c52
Thomas Mathieu <mathieuthomas84300@hotmail.fr>
2019-03-12 17:09:38 +0100

    add getter and setter for name

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -3,4 +3,27 @@ package Game;
 public class Player 
 {
 	String name ;
+
+	public Player(String name) 
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() 
+	{
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	
 }
