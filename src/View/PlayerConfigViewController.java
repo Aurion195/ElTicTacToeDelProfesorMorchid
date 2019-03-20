4944d5d2d1d44b731f0ac9f52a4dbc5ceed3e189
Aurion195 <mathieuthomas84300@hotmail.fr>
2019-03-20 10:17:54 +0100

    Add PlayerConfigViewController java

---
+++ src/View/PlayerConfigViewController.java
@@ -0,0 +1,31 @@
+package View;
+
+import javafx.fxml.FXML;
+
+import Game.Launcher;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+
+public class PlayerConfigViewController extends ToolsBarController 
+{
+	@FXML
+	public void onClickQuitButton()
+	{
+		super.onClickQuitButton();
+	}
+	
+	@FXML
+	public void onClickHomeButton()
+	{
+		super.onClickHomeButton();
+	}
+	
+	@FXML
+	public void onClickSettingButton()
+	{
+		super.onClickSettingButton();
+	}
+}
