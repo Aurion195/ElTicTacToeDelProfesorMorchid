e7f9ed1867df2c0dab291e1b229e22b512a7b106
uapv1703778 <uapv1703778@ge-st3-103.univ-avignon.fr>
2019-04-03 15:52:22 +0200

    add Javadoc

--- src/View/IAViewController.java
+++ src/View/IAViewController.java
@@ -1,5 +1,11 @@
 package View;
 
+/**
+ * Classe permettant de gérer le solo VS IA 
+ * @author MATHIEU Thomas
+ * @author GARCIA Jérémy
+ *
+ */
 public class IAViewController extends ToolsBarController
 {
 	
